09c73f0177caeb63ef1dd4ce059a6bb1a75cb7be
nmarhari <nassimmarhari@gmail.com>
2023-11-30 17:47:55 -0500

    php no auth

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html>
+    
+    <head>
+
+        <title>Charge!</title>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<link rel="stylesheet" href="./css/style.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+    </head>
+
+    <div class="login-page">
+
+        <div class="container">
+            <p> test </p>
+        </div>
+
+    </div>
+
+
+</html>
